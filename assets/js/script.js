e040b1c5f677ff523cb0d1177188fd57a69880ea
Mo Yang <moyang313@gmail.com>
2022-02-27 13:52:25 -0500

    added function to generate text in the password box

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,25 +2,84 @@
 
 //Define variables for user input
 var length = "";
-var lowercase = "";
-var uppercase = "";
-var numbers = "";
-var special = "";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "1234567890";
+var special = "!@#$%^&*_-+=";
 
-length = prompt("Please pick a password length of at least 8 characters and no more than 128 characters");
-console.log(length);
 
-lowercase = window.confirm("Would you like to include lowercase characters?");
-console.log(lowercase);
+// length = prompt("Please pick a password length of at least 8 characters and no more than 128 characters");
+// console.log(length);
 
-uppercase = window.confirm("Would you like to include uppercase characters?");
-console.log(uppercase);
+// lowercase = window.confirm("Would you like to include lowercase characters?");
+// console.log(lowercase);
 
-numbers = window.confirm("Would you like to include numbers?");
-console.log(numbers);
+// uppercase = window.confirm("Would you like to include uppercase characters?");
+// console.log(uppercase);
 
-special = window.confirm("Would you like to include special characters?");
-console.log(special);
+// numbers = window.confirm("Would you like to include numbers?");
+// console.log(numbers);
+
+// special = window.confirm("Would you like to include special characters?");
+// console.log(special);
+
+
+// function to generate a random numeric value
+// var randomNumber = function(min, max) {
+//   var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+//   return value;
+// };
+
+// const generatePassword = (length, characters) => {
+//   let password = "";
+//   for (let i = 0; i < length; i++) {
+//     password += characters.charAt(
+//       Math.floor(Math.random() * characters.length)
+//     );
+//   }
+//   return password;
+// };
+
+// Run prompts on button click
+function generatePassword () {
+  
+  length = prompt("Please pick a password length of at least 8 characters and no more than 128 characters");
+  console.log(length);
+
+  var checkLower = window.confirm("Would you like to include lowercase characters?");
+  if (checkLower) {
+    return lowercase;
+  }
+  else {
+    window.alert("Invalid Input");
+  }
+
+  var checkUpper = window.confirm("Would you like to include uppercase characters?");
+  if (checkUpper) {
+    return uppercase;
+  }
+  else {
+    window.alert("Invalid Input");
+  }
+
+  var checkNumber = window.confirm("Would you like to include numbers?");
+  if (checkNumber) {
+    return numbers;
+  }
+  else {
+    window.alert("Invalid Input");
+  }
+
+  var checkSpecial = window.confirm("Would you like to include special characters?");
+  if (checkSpecial) {
+    return special;
+  }
+  else {
+    window.alert("Invalid Input");
+  }
+}
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
