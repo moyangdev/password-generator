5266c6c5c4d778639d075afcb9b57e82470b820c
Mo Yang <moyang313@gmail.com>
2022-02-27 12:11:16 -0500

    added prompts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,26 @@
 // Assignment code here
 
+//Define variables for user input
+var length = "";
+var lowercase = "";
+var uppercase = "";
+var numbers = "";
+var special = "";
+
+length = prompt("Please pick a password length of at least 8 characters and no more than 128 characters");
+console.log(length);
+
+lowercase = window.confirm("Would you like to include lowercase characters?");
+console.log(lowercase);
+
+uppercase = window.confirm("Would you like to include uppercase characters?");
+console.log(uppercase);
+
+numbers = window.confirm("Would you like to include numbers?");
+console.log(numbers);
+
+special = window.confirm("Would you like to include special characters?");
+console.log(special);
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
